2ede83e2b4dc74093ce7e1f55297c42d4c86a549
patosz <patrick.dolega@outlook.com>
2015-12-12 22:23:49 +0100

    Début UI

--- MafiaBoardGame/UI/Controllers/JoueurController.cs
+++ MafiaBoardGame/UI/Controllers/JoueurController.cs
@@ -3,16 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using Domain.Model;
 
 namespace UI.Controllers
 {
-    public class IndexController : Controller
+    public class JoueurController : Controller
     {
-        // GET: Index
-        public ActionResult Index(Joueur j)
+
+        public ActionResult Inscrire()
         {
-            return View(j);
+
+            return RedirectToAction("Partie",new { controller = "Index" });
         }
     }
 }
