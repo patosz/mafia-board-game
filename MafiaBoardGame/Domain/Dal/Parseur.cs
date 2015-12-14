176bf272d4a960a5137323883eae39ee519e222b
Milenovorkapic <milenkovorkapic@gmail.com>
2015-12-14 14:33:22 +0100

    Parseur dico

--- MafiaBoardGame/Domain/Dal/Parseur.cs
+++ MafiaBoardGame/Domain/Dal/Parseur.cs
@@ -14,8 +14,32 @@ namespace Domain.Dal
     public class Parseur
     {
         //Load xml
-        private XDocument xdoc = XDocument.Load(HostingEnvironment.ApplicationPhysicalPath+"Xml/wazabi.xml");
+        private XDocument xdoc = XDocument.Load(HostingEnvironment.ApplicationPhysicalPath + "Xml/wazabi.xml");
 
+
+        public Dictionary<string, int> loadInfos()
+        {
+            Dictionary<string, int> dico = new Dictionary<string, int>();
+            XElement cartes = (from xml2 in xdoc.Elements("wazabi")
+                              select xml2).FirstOrDefault();
+            dico.Add(cartes.Attribute("nbCartesParJoueur").Name.ToString(),int.Parse(cartes.Attribute("nbCartesParJoueur").Value));
+            dico.Add(cartes.Attribute("nbCartesTotal").Name.ToString(), int.Parse(cartes.Attribute("nbCartesTotal").Value));
+            dico.Add(cartes.Attribute("minJoueurs").Name.ToString(), int.Parse(cartes.Attribute("minJoueurs").Value));
+            dico.Add(cartes.Attribute("maxJoueurs").Name.ToString(), int.Parse(cartes.Attribute("maxJoueurs").Value));
+
+            XElement des = (from xml in xdoc.Elements("de")
+                            select xml).FirstOrDefault();
+            dico.Add(des.Attribute("nbParJoueur").Name.ToString(), int.Parse(des.Attribute("nbParJoueur").Value));
+            dico.Add(des.Attribute("nbTotalDes").Name.ToString(), int.Parse(des.Attribute("nbTotalDes").Value));
+
+            IEnumerable<XElement> faces = from face in xdoc.Descendants("face") select face;
+            foreach(var face in faces)
+            {
+                dico.Add(face.Attribute("figure").Value, int.Parse(face.Attribute("nbFaces").Value));
+            }
+
+            return dico;
+        }
         public List<Carte> loadCarte()
         {
 
@@ -27,12 +51,12 @@ namespace Domain.Dal
             {
                 int nombreCarte = int.Parse(carte.Attribute("nb").Value);
 
-                for(int i = 0; i < nombreCarte; i++)
+                for (int i = 0; i < nombreCarte; i++)
                 {
                     list.Add(new Carte(int.Parse(carte.Attribute("codeEffet").Value), int.Parse(carte.Attribute("cout").Value), carte.Attribute("effet").Value, carte.Value));
                 }
 
-                
+
                 //Console.WriteLine(carte);
             }
             return list;
@@ -43,12 +67,24 @@ namespace Domain.Dal
                                select xml2).FirstOrDefault();
 
             List<int> list = new List<int>();
-            
+
             list.Add(int.Parse(cartes.Attribute("nbCartesParJoueur").Value));
             list.Add(int.Parse(cartes.Attribute("nbCartesTotal").Value));
             list.Add(int.Parse(cartes.Attribute("minJoueurs").Value));
             list.Add(int.Parse(cartes.Attribute("maxJoueurs").Value));
             return list;
         }
+
+        public List<int> loadInfosDes()
+        {
+            List<int> list = new List<int>();
+            XElement des = (from xml in xdoc.Elements("de")
+                            select xml).FirstOrDefault();
+            list.Add(int.Parse(des.Attribute("nbParJoueur").Value));
+            list.Add(int.Parse(des.Attribute("nbTotalDes").Value));
+            return list;
+        }
+
+
     }
 }
