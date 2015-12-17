f95ff953498a22af8e460cc1c28bd4407e978ab4
patosz <patrick.dolega@outlook.com>
2015-12-17 09:57:01 +0100

    dl projet

---
+++ MafiaBoardGame/Domain/GestionJoueurPartieImpl.svc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Domain
+{
+    // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom de classe "GestionJoueurPartieImpl" à la fois dans le code, le fichier svc et le fichier de configuration.
+    // REMARQUE : pour lancer le client test WCF afin de tester ce service, sélectionnez GestionJoueurPartieImpl.svc ou GestionJoueurPartieImpl.svc.cs dans l'Explorateur de solutions et démarrez le débogage.
+    public class GestionJoueurPartieImpl : IGestionJoueurPartie
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
