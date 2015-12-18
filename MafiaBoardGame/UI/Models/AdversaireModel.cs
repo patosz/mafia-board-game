bf76db0537e5071e716a24195731a359a6ed1fad
patosz <patrick.dolega@outlook.com>
2015-12-18 09:46:52 +0100

    modele plateau

---
+++ MafiaBoardGame/UI/Models/AdversaireModel.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UI.Models
+{
+    public class AdversaireModel
+    {
+        public string Pseudo { get; set; }
+
+        public List<CarteDto> Cartes { get; set; }
+
+        public List<DeDto> Des { get; set; }
+
+    }
+}
---
+++ MafiaBoardGame/UI/Models/ModelPlateau.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UI.Models
+{
+    public class ModelPlateau
+    {
+
+
+        public string JoueurCourant { get; set; }
+
+        public List<AdversaireModel> Adversaires { get; set; }
+
+        public List<CarteDto> MesCartes { get; set; }
+
+        public List<DeDto> MesDes { get; set; }
+
+        public ETAT_PARTIE Etat { get; set; }
+
+        public CarteDto DerniereCarteJouee { get; set; }
+
+    }
+
+}
